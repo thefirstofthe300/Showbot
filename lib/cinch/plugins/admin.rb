3256d577250c20a6fcfa24201064b376899d945d
Richard Korman <winter.blood@gmail.com>
2015-04-10 13:27:52 -0700

    Simple authed join/part commands
    These can be useful for bringing the bot elsewhere wasily. :)

--- lib/cinch/plugins/admin.rb
+++ lib/cinch/plugins/admin.rb
@@ -8,12 +8,33 @@ module Cinch
       match /(?:exit|quit)/i,     :method => :command_exit
       match /start_show\s+(.+)/i, :method => :command_start_show
       match /end_show/i,          :method => :command_end_show
+      match /join\s(.+)/i,        :method => :command_join
+      match /leave\s(.+)/i,       :method => :command_leave
 
       def initialize(*args)
         super
         @admins = Array(config[:admins])
         @data_json = DataJSON.new config[:data_json]
       end
+
+      def command_join(m, channel)
+        if !authed? m.user
+          m.user.send 'You are not authorized to invite the bot.'
+          return
+        end
+
+        Channel(channel).join
+      end
+
+      def command_leave(m, channel)
+        if !authed? m.user
+          m.user.send 'You are not authorized to make the bot leave.'
+          return
+        end
+
+        Channel(channel).part
+      end
+
 
       def command_start_show(m, show_slug)
         if !authed? m.user
