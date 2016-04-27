839feb0383d88e0634e215e1b854351de1b2243a
Erik Nelson <erik@nsk.io>
2016-04-26 22:14:40 -0400

    Initial admin droplet commands

--- lib/cinch/plugins/admin.rb
+++ lib/cinch/plugins/admin.rb
@@ -5,17 +5,22 @@ module Cinch
     class Admin
       include Cinch::Plugin
 
-      match /(?:exit|quit)/i,     :method => :command_exit
-      match /start_show$/i,       :method => :command_show_list
-      match /start_show\s+(.+)/i, :method => :command_start_show
-      match /end_show/i,          :method => :command_end_show
-      match /join\s(.+)/i,        :method => :command_join
-      match /leave\s(.+)/i,       :method => :command_leave
+      match /(?:exit|quit)/i,           :method => :command_exit
+      match /start_show$/i,             :method => :command_show_list
+      match /start_show\s+(.+)/i,       :method => :command_start_show
+      match /end_show/i,                :method => :command_end_show
+      match /join\s(.+)/i,              :method => :command_join
+      match /leave\s(.+)/i,             :method => :command_leave
+      match /hi$/i,                     :method => :command_sayhi
+      match /droplet\slist$/i,          :method => :command_droplet_list
+      match /droplet\sstart\s(.+)/i,    :method => :command_droplet_start
+      match /droplet\sstop\s(.+)/i,     :method => :command_droplet_stop
 
       def initialize(*args)
         super
         @admins = Array(config[:admins])
         @data_json = DataJSON.new config[:data_json]
+        @do_client = DropletKit::Client.new(access_token: ENV['DO_API_KEY']) if ENV['DO_API_KEY']
       end
 
       def command_join(m, channel)
@@ -109,6 +114,61 @@ module Cinch
           channel.send "#{shared[:Bot_Nick]} is shutting down. Good bye :("
         end
         Process.exit
+      end
+
+      ############################################################
+      # Digital Ocean Hooks
+      # TODO: Deserves its own module
+      # TODO: Error hardening
+      # TODO: Force stop?
+      ############################################################
+      def command_droplet_list(m)
+        if !authed? m.user
+          m.user.send 'You are not authorized for droplet access.'
+          return
+        end
+
+        m.user.send '=============================='
+        m.user.send 'Listing known droplets...'
+        m.user.send '=============================='
+
+        @do_client.droplets.all.each do |droplet|
+          m.user.send "[#{droplet[:name]}]"
+          m.user.send "  id: #{droplet[:id]}"
+          m.user.send "  status: #{droplet[:status]}"
+        end
+
+        m.user.send '=============================='
+        m.user.send 'Droplet list complete.'
+        m.user.send '=============================='
+      end
+
+      def command_droplet_start(m, droplet_id)
+        if !authed? m.user
+          m.user.send 'You are not authorized for droplet access.'
+          return
+        end
+
+        begin
+          @do_client.droplet_actions.power_on(droplet_id: droplet_id)
+          m.user.send "Request to start droplet #{droplet_id} succeeded!"
+        rescue
+          m.user.send 'An error occurred requesting the droplet to stop. Is your ID correct?'
+        end
+      end
+
+      def command_droplet_stop(m, droplet_id)
+        if !authed? m.user
+          m.user.send 'You are not authorized for droplet access.'
+          return
+        end
+
+        begin
+          @do_client.droplet_actions.shutdown(droplet_id: droplet_id)
+          m.user.send "Request to stop droplet #{droplet_id} succeeded!"
+        rescue
+          m.user.send 'An error occurred requesting the droplet to stop. Is your ID correct?'
+        end
       end
 
       private
